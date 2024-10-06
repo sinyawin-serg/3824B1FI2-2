2fa9b13b9fed74aaa60e7d88ee19a13915a8b2ce
sofiasheburova <sofyasheburova@gmail.com>
2024-10-06 21:42:09 +0300

    $ git status prog0 $ git add prog0 $ git commit -m "lab1/prog0" $ git push sofiasheburova/3824B1FI2-2

--- lab1/prog0.cpp
+++ lab1/prog0.cpp
@@ -1,30 +1,27 @@
 #include <stdio.h>
 #include <locale.h>
-int main(){
+#include <stdlib.h>
+#include <time.h>
+int main() {
+    srand(time(NULL));
+    int number = 1 + (rand() % 10);
     setlocale (LC_ALL,"Russian");
-    float h, w, a, m1, m2, m3,V, M, pd = 0.6, pdsp = 0.7, pdvp = 1.0, t1 = 0.5, t2 = 1.5, t3 = 1.0;
-//h,w,a,V - высота, ширина , глубина, объем
-//m3,m1,m2,M - масса оставшихся часей, м стенки cзади, м дверей,м всего
-//pd,pdvp,t1,t2,t3 - постоянные плотности дерева, ДВП,толщины
-    int d,b;
-    printf("Введите высоту,ширину задней стены, глубину боковой\n");
-    scanf_s("%f %f %f",&h, &w, &a);
-    if ((h > 220) || (h < 180) || (w > 120) || (w < 80) || (a > 90) || (a < 50)) {
-        printf("Неверные данные");
-    }else{
-        d = h/40;
-        m1 = h*w*t1*pdvp;
-        m2 = h*w*t3*pd;
-        m3 = 2*h*a*t2*pdsp + 2*w*a*t2*pdsp+d*w*a*t2*pdsp;
-        M = (m1+m2+m3)/1000;
-        b = M;
-        printf("Шкаф весит: %d кг (%1.f)%f",b,M,M);
+    int our_number = 11,k = 0;
+    while (our_number!=number){
+        printf("Введи номер\n");
+        scanf_s("%d",&our_number);
+        k++;
+        if (our_number!=number){
+            if (our_number > number){
+            printf("Номер меньше\n");}
 
+            
+            else
+            printf("Номер больше\n");}
+        
+        
+        }
+            printf("Победа\n");
+            printf("Попыток-%d",k);
+        return 0;
     }
-    return 0;
-}
-
-
-
-
-
