fb56d7fc7aa933ed68e2c79fdba2fe62a7049055
DaniFrantz <francevd7@gmail.com>
2024-10-08 09:56:20 +0300

    .

--- FRANTSEV_DE/lab2/Lab2.cpp
+++ FRANTSEV_DE/lab2/Lab2.cpp
@@ -13,7 +13,7 @@ int main()
         srand(time(NULL));
         int THINK = 1 + (rand() % 1000);
         int YOUTHINK = 1002;
-        printf("Число от 1 до 1000 загадано. Игра начинается...\n%d\n", THINK);
+        printf("Число от 1 до 1000 загадано. Игра начинается...\n\n");
         while (YOUTHINK != THINK){
             printf("Введите число ==> ");
             scanf_s("%d", &YOUTHINK);
@@ -25,7 +25,7 @@ int main()
         printf("Вы угадали!\n");
     }
     else if (mode == 2) {
-
+        system("cls");
         int think = 500, down = 0, up = 1000;
         int youthink, counter = 1;
         char oper[2];
@@ -48,10 +48,11 @@ int main()
                     up = think;
                     think = (think + down) / 2;
                 }
-                else {
+                else if (oper[0] == '=') {
                     printf("Ты поддался... Спасибо?\n");
-                    return 0;
                 }
+                else {printf("\n\n\nТакого действия нет, барашек\n\n\n"); return 0;
+            }
             }
             if (think == youthink) {
                 printf("Число %d больше, меньше или равно?\n", think);
@@ -60,6 +61,7 @@ int main()
                 while (getchar() != '\n');
                 if (oper[0] == '=') { printf("Победа\n"); }
                 else printf("Грязный лжец.");
+             
             }
             printf("\n\nПопыток на угадывание вашего числа: %d\n\n", counter);
         }
