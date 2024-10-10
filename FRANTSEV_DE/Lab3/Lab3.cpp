2d38f3c63ce7e1908c27b5f3b008441e685b9ba9
DaniFrantz <francevd7@gmail.com>
2024-10-10 13:06:59 +0300

    Я сделал Быков и Коров за 30 минут и с меньшим количеством строк! Ура!

--- FRANTSEV_DE/Lab3/Lab3.cpp
+++ FRANTSEV_DE/Lab3/Lab3.cpp
@@ -1,6 +1,7 @@
 ﻿#include <stdio.h>
 #include <locale.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 
@@ -8,7 +9,7 @@ int main()
 {
     int mode;
     setlocale(LC_ALL, "rus");
-    printf("Выберите задачу:\n1 - НОД\n2 - Сумма чисел\n3 - Найти по сумме чисел\n4 - Wordly, Lab3\n");
+    printf("Выберите задачу:\n1 - НОД\n2 - Сумма чисел\n3 - Найти по сумме чисел\n4 - Wordly, Lab3\n5 - Быки и коровы\n");
     scanf_s("%d", &mode);
     if (mode == 1) {
         system("cls");
@@ -101,7 +102,64 @@ int main()
             } 
         if (sovpadenia == length) printf("\nВы угадали!\n");
         }
-    
+    else if (mode == 5) {
+        system("cls");
+        char c[6] = {"\0"}, d[6];
+        int length, popitki, nageneril, raz, bebra, sovpadenia = 0, counter = -1, counter1 = 0, counterx2 = -1, bebra2 = 0;
+        srand(time(NULL));
+        printf("Введите длину числа: \n");
+        scanf_s("%d", &length);
+        printf("Введите количество попыток: \n");
+        scanf_s("%d", &popitki);
+        c[0] = 48+(1 + (rand() % 9));
+        for (int i = 1; i < length; i++) {
+            bebra = 0;
+            while (bebra == 0) {
+                raz = 0;
+                nageneril = 48+ (rand() % 10);
+                for (int l = 0; l < i; l++) {
+                    if (c[l] != nageneril) { raz++; }
+                }
+                if (raz == i) {
+                    c[i] = nageneril;
+                    bebra = 1;
+                }
+            }
+        }
+        while (getchar() != '\n');
+        system("cls");
+        while (sovpadenia != length) {
+            sovpadenia = 0;
+            if (popitki != 0) {
+                printf("\nПопытайтесь угадать число с количеством цифр: %d\n", length);
+                printf("Осталось попыток: %d\n", popitki);
+                fgets(d, length + 1, stdin);
+                while (getchar() != '\n');
+                counter = -1;
+                counter1 = 0;
+                while (counter < length - 1) {
+                    counter++;
+                    counterx2 = -1;
+                    counter1++;
+                    bebra2 = 0;
+                    if ((c[counter] == d[counter]) && (c[counter] != '\n')) { printf("%d - совпадение\n", counter1); sovpadenia += 1; bebra2 = 1; }
+                    else {
+                        while (counterx2 < length - 1) {
+                            counterx2++;
+                            if (d[counter] == c[counterx2]) { printf("%d - присутствие\n", counter1); bebra2 = 1; break; }
+                        }
+                    }
+                    if (bebra2 == 0) printf("%d - отсутствие\n", counter1);
+                }
+                popitki -= 1;
+
+
+
+            }
+            else { printf("\nПопытки кончились, вы проиграли!\n"); break; }
+        }
+        if (sovpadenia == length) printf("\nВы угадали!\n");
+    }
     else printf("PIPISKA\n");
 }
 
